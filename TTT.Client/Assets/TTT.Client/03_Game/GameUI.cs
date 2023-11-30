3f627a01598f429c9faef815bbee737d504d5fec
Igor Semenov <igor.semenov.minsk@gmail.com>
2023-11-30 13:10:42 +0100

    Game UI

---
+++ TTT.Client/Assets/TTT.Client/03_Game/GameUI.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using TTT.Client.Gameplay;
+using UnityEngine;
+using VContainer;
+
+namespace TTT.Client.Game
+{
+    public class GameUI : MonoBehaviour
+    {
+        [Header("Header:")]
+        [SerializeField]
+        private UserView xUserView;
+        
+        [SerializeField]
+        private UserView oUserView;
+
+        private IGameManager gameManager;
+
+        [Inject]
+        public void Construct(IGameManager gameManager)
+        {
+            this.gameManager = gameManager;
+        }
+
+        private void Start()
+        {
+            InitHeader();
+        }
+
+        private void InitHeader()
+        {
+            var activeGame = gameManager.ActiveGame;
+            xUserView.SetUser(activeGame.XUser, 0);
+            oUserView.SetUser(activeGame.OUser, 0);
+        }
+    }
+}
---
+++ TTT.Client/Assets/TTT.Client/03_Game/UserView.cs
@@ -0,0 +1,20 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace TTT.Client.Game
+{
+    public class UserView : MonoBehaviour
+    {
+        [SerializeField]
+        private TextMeshProUGUI userName;
+        
+        [SerializeField]
+        private TextMeshProUGUI score;
+        
+        public void SetUser(string userName, int score)
+        {
+            this.userName.text = userName;
+            this.score.text = score.ToString();
+        }
+    }
+}
