03618f93de31c752d42f5f50f717bd0d8d4e11b1
Igor Semenov <igor.semenov.minsk@gmail.com>
2023-12-11 11:58:19 +0100

    Refactoring

--- TTT.Client/Assets/TTT.Client/UIAnimations/MoveAnimation.cs
+++ TTT.Client/Assets/TTT.Client/UIAnimations/MoveAnimation.cs
@@ -1,80 +1,17 @@
-using DG.Tweening;
 using TriInspector;
+using TTT.Client.UIAnimations;
 using UnityEngine;
 
 [DeclareFoldoutGroup("Debug")]
-public class MoveAnimation : MonoBehaviour
+public class MoveAnimation : UIAnimation
 {
-    [SerializeField]
-    private bool animateOnStart = true;
-
-    [SerializeField]
-    [Tooltip("If empty then use self")]
-    private RectTransform animationTarget;
-
-    [Header("Animation:")]
-    [SerializeField]
-    private float animationDuration = 0.3f;
-
-    [SerializeField]
-    private Ease animationEase = Ease.OutCubic;
-
     [SerializeField]
     private RectTransform startPosition;
 
     [SerializeField]
     private RectTransform endPosition;
 
-    private Tweener moveAnimation;
-
-    private RectTransform AnimationTarget { get; set; }
-
-    private void Awake()
-    {
-        InitAnimationTarget();
-        CreateAnimation();
-
-        if (animateOnStart)
-        {
-            PlayShow();
-        }
-    }
-
-    private void OnDestroy()
-    {
-        DOTween.Kill(moveAnimation);
-    }
-
-    [Button]
-    [Group("Debug")]
-    public void PlayShow()
-    {
-        moveAnimation.PlayForward();
-    }
-
-    [Button]
-    [Group("Debug")]
-    public void PlayHide()
-    {
-        moveAnimation.PlayBackwards();
-    }
-
-    private void InitAnimationTarget()
-    {
-        AnimationTarget = animationTarget ? animationTarget : (RectTransform) transform;
-    }
-
-    private void CreateAnimation()
-    {
-        moveAnimation = DOTween.To(Move, 0f, 1f, animationDuration)
-                               .SetEase(animationEase)
-                               .SetLink(gameObject)
-                               .SetAutoKill(false);
-
-        moveAnimation.Rewind();
-    }
-
-    private void Move(float progress)
+    protected override void Animate(float progress)
     {
         AnimationTarget.position = Vector3.LerpUnclamped(
             startPosition.position,
---
+++ TTT.Client/Assets/TTT.Client/UIAnimations/UIAnimation.cs
@@ -0,0 +1,86 @@
+﻿using Cysharp.Threading.Tasks;
+using DG.Tweening;
+using TriInspector;
+using UnityEngine;
+
+namespace TTT.Client.UIAnimations
+{
+    public abstract class UIAnimation : MonoBehaviour
+    {
+        [SerializeField]
+        [Tooltip("If empty then use self")]
+        private RectTransform animationTarget;
+
+        [Header("Start:")]
+        [SerializeField]
+        private bool animateOnStart = true;
+
+        [SerializeField]
+        [ShowIf(nameof(animateOnStart))]
+        private float startDelay;
+
+        [Header("Animation:")]
+        [SerializeField]
+        private float animationDuration = 0.3f;
+
+        [SerializeField]
+        private Ease animationEase = Ease.OutCubic;
+
+        private Tweener uiAnimation;
+
+        protected RectTransform AnimationTarget { get; private set; }
+
+        private async UniTaskVoid Awake()
+        {
+            InitAnimationTarget();
+            CreateAnimation();
+
+            if (animateOnStart)
+            {
+                if (startDelay > 0)
+                {
+                    await UniTask.Delay((int) (startDelay * 1000), cancellationToken: destroyCancellationToken);
+                    destroyCancellationToken.ThrowIfCancellationRequested();
+                }
+
+                PlayForward();
+            }
+        }
+
+        private void OnDestroy()
+        {
+            DOTween.Kill(uiAnimation);
+        }
+
+        [Button]
+        [Group("Debug")]
+        public void PlayForward()
+        {
+            uiAnimation.PlayForward();
+        }
+
+        [Button]
+        [Group("Debug")]
+        public void PlayBackward()
+        {
+            uiAnimation.PlayBackwards();
+        }
+
+        protected abstract void Animate(float progress);
+
+        private void InitAnimationTarget()
+        {
+            AnimationTarget = animationTarget ? animationTarget : (RectTransform) transform;
+        }
+
+        private void CreateAnimation()
+        {
+            uiAnimation = DOTween.To(Animate, 0f, 1f, animationDuration)
+                                 .SetEase(animationEase)
+                                 .SetLink(gameObject)
+                                 .SetAutoKill(false);
+
+            uiAnimation.Rewind();
+        }
+    }
+}
